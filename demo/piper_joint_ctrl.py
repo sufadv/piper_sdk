550ffc80f74e82c9de514ddfe443d62dacc6fcd8
Akshay Kashyap <akshay@sufadv.com>
2024-10-30 13:47:29 -0700

    disable on interruopt

--- demo/piper_joint_ctrl.py
+++ demo/piper_joint_ctrl.py
@@ -15,30 +15,31 @@ if __name__ == "__main__":
     factor = 57324.840764 #1000*180/3.14
     position = [0,0,0,0,0,0,0]
     count = 0
-    while True:
-        import time
-        count  = count + 1
-        print(count)
-        if(count == 0):
-            print("1-----------")
-            position = [0,0,0,0,0,0,0]
-        elif(count == 500):
-            print("2-----------")
-            position = [0,0,0,0,0,0,0.08]
-        elif(count == 1000):
-            position = [0,0,0,0,0,0,0]
-            count = 0
-        
-        joint_0 = round(position[0]*factor)
-        joint_1 = round(position[1]*factor)
-        joint_2 = round(position[2]*factor)
-        joint_3 = round(position[3]*factor)
-        joint_4 = round(position[4]*factor)
-        joint_5 = round(position[5]*factor)
-        joint_6 = round(position[6]*1000*1000)
-        piper.MotionCtrl_2(0x01, 0x01, 50)
-        piper.JointCtrl(joint_0, joint_1, joint_2, joint_3, joint_4, joint_5)
-        piper.GripperCtrl(abs(joint_6), 1000, 0x01, 0)
-        piper.MotionCtrl_2(0x01, 0x01, 50)
-        time.sleep(0.005)
-        pass
+    try:
+        while True:
+            import time
+            count  = count + 1
+            if(count == 0):
+                print("1-----------")
+                position = [0,0,0,0,0,0,0]
+            elif(count == 500):
+                print("2-----------")
+                position = [0,0,0,0,0,0,0]
+            elif(count == 1000):
+                position = [0,0,0,0,0,0,0]
+                count = 0
+            
+            joint_0 = round(position[0]*factor)
+            joint_1 = round(position[1]*factor)
+            joint_2 = round(position[2]*factor)
+            joint_3 = round(position[3]*factor)
+            joint_4 = round(position[4]*factor)
+            joint_5 = round(position[5]*factor)
+            joint_6 = round(position[6]*1000*1000)
+            piper.MotionCtrl_2(0x01, 0x01, 50)
+            piper.JointCtrl(joint_0, joint_1, joint_2, joint_3, joint_4, joint_5)
+            piper.GripperCtrl(abs(joint_6), 1000, 0x01, 0)
+            piper.MotionCtrl_2(0x01, 0x01, 50)
+            time.sleep(0.005)
+    except KeyboardInterrupt:
+        piper.DisableArm(7)
